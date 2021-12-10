8fbca343413eaed9c6138e892759b395ca957553
RoneyThomas <roneythomas6@gmail.com>
2021-12-10 04:30:15 -0500

    More permisions

--- express_server.js
+++ express_server.js
@@ -48,9 +48,9 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  let templateVars = {};
   if (req.cookies['user_id'] !== undefined) {
-    templateVars['users'] = users[req.cookies['user_id']];
+    templateVars = { urls: urlsForUser(req.cookies['user_id']), users: users[req.cookies['user_id']] };
   }
   res.render('urls_index', templateVars);
 });
@@ -94,7 +94,7 @@ app.post('/urls', (req, res) => {
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   if (req.cookies['user_id'] !== undefined && users[req.cookies['user_id']] !== undefined) {
-    if (urlDatabase[req.params.shortURL] !== undefined) {
+    if (urlDatabase[req.params.shortURL].userID === req.cookies['user_id']) {
       console.log(`Deleted ${req.params.shortURL} : ${urlDatabase[req.params.shortURL]}`);
       delete urlDatabase[req.params.shortURL];
     }
@@ -104,7 +104,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 app.get('/urls/:shortURL', (req, res) => {
   if (req.cookies['user_id'] !== undefined && users[req.cookies['user_id']] !== undefined) {
-    if (urlDatabase[req.params.shortURL] !== undefined && urlDatabase[req.params.shortURL].userID === req.cookies['user_id']) {
+    if (urlDatabase[req.params.shortURL].userID === req.cookies['user_id']) {
       const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, email: users[req.cookies['user_id']].id };
       res.render('urls_show', templateVars);
     } else {
@@ -160,7 +160,7 @@ app.post('/register', (req, res) => {
     if (checkEmailExists(req.body.email)) {
       res.status(400).send('Email already registered');
     } else {
-      const randomID = `user${generateRandomString()}`;
+      const randomID = generateRandomString();
       users[randomID] = {
         id: randomID,
         email: req.body.email,
@@ -168,7 +168,7 @@ app.post('/register', (req, res) => {
       };
       res.cookie('user_id', randomID);
       // console.log(users);
-      res.redirect(`/urls`);
+      res.redirect('/urls');
     }
   }
 });
@@ -182,10 +182,20 @@ const checkEmailExists = (email) => {
   return undefined;
 };
 
+const urlsForUser = (id) => {
+  let urlFiltered = {};
+  for (const [uKey, uValue] of Object.entries(urlDatabase)) {
+    if (uValue.userID === id) {
+      urlFiltered[uKey] = uValue;
+    }
+  }
+  return urlFiltered;
+};
+
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
 });
 
 const generateRandomString = () => {
-  return crypto.randomBytes(3).toString('hex');
+  return crypto.randomBytes(4).toString('hex');
 };
