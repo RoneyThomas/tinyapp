530d87e317da47f0945ebd3a2581ce6314f51fb4
RoneyThomas <roneythomas6@gmail.com>
2021-12-11 04:25:52 -0500

    Added screenshot

--- express_server.js
+++ express_server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 const crypto = require('crypto');
 const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs');
+const methodOverride = require('method-override');
+
+
+const { getUserByEmail, urlsForUser } = require('./helper');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
-const bcrypt = require('bcryptjs');
-const { getUserByEmail, urlsForUser } = require('./helper');
-const PORT = 8080;
-
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
-// const urlDatabase = {
-//   'b2xVn2': 'http://www.lighthouselabs.ca',
-//   '9sm5xK': 'http://www.google.com'
-// };
+const PORT = 8080;
 
 const urlDatabase = {
   b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
+    longURL: 'https://www.tsn.ca',
+    userID: 'aJ48lW'
   },
   i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW"
+    longURL: 'https://www.google.ca',
+    userID: 'aJ48lW'
   }
 };
 
@@ -45,7 +45,11 @@ const users = {
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
+    res.redirect('/urls/');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get('/hello', (req, res) => {
@@ -68,20 +72,33 @@ app.get('/urls.json', (req, res) => {
 app.get('/urls/new', (req, res) => {
   const templateVars = { urls: urlDatabase };
   if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
-    templateVars['users'] = users[req.session.userID];
+    templateVars.users = users[req.session.userID];
     res.render('urls_new', templateVars);
   } else {
     res.redirect('/login');
   }
 });
 
-app.post('/urls/:id', (req, res) => {
+app.put('/urls/:id', (req, res) => {
   if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
-    if (urlDatabase[req.params.id].userID === req.session.userID) {
+    if (urlDatabase[req.params.id] !== undefined && urlDatabase[req.params.id].userID === req.session.userID) {
       urlDatabase[req.params.id].longURL = req.body.longURL;
+      res.redirect(`/urls`);
+    } else {
+      // user is logged it but does not own the URL with the given ID
+      const errorMessage = {
+        users: users[req.session.userID],
+        heading: `No URL found for ${users[req.session.userID].email}`
+      };
+      res.render('url_not_found', errorMessage);
     }
+  } else {
+    // user is not logged in
+    const errorMessage = {
+      heading: 'Not Logged In'
+    };
+    res.render('url_not_found', errorMessage);
   }
-  res.redirect(`/urls`);
 });
 
 app.post('/urls', (req, res) => {
@@ -94,30 +111,52 @@ app.post('/urls', (req, res) => {
     };
     res.redirect(`/urls/${urlDBKey}`);
   } else {
+    // If the user is not logged in
     res.redirect('/login');
   }
 });
 
-app.post('/urls/:shortURL/delete', (req, res) => {
+app.delete('/urls/:shortURL', (req, res) => {
   if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
     if (urlDatabase[req.params.shortURL].userID === req.session.userID) {
       console.log(`Deleted ${req.params.shortURL} : ${urlDatabase[req.params.shortURL]}`);
       delete urlDatabase[req.params.shortURL];
+      res.redirect('/urls');
+    } else {
+      // user is logged it but does not own the URL with the given ID
+      const errorMessage = {
+        heading: `No URL found for ${users[req.session.userID].email}`
+      };
+      res.render('url_not_found', errorMessage);
     }
+  } else {
+    // user is not logged in
+    const errorMessage = {
+      heading: 'Not Logged In'
+    };
+    res.render('url_not_found', errorMessage);
   }
-  res.redirect('/urls');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
-    if (urlDatabase[req.params.shortURL].userID === req.session.userID) {
-      const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, email: users[req.session.userID].id };
+    if (urlDatabase[req.params.shortURL] !== undefined && urlDatabase[req.params.shortURL].userID === req.session.userID) {
+      const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, users: users[req.session.userID] };
       res.render('urls_show', templateVars);
     } else {
-      res.redirect('/urls');
+      // user is logged it but does not own the URL with the given ID
+      const errorMessage = {
+        users: users[req.session.userID],
+        heading: `No URL found for ${users[req.session.userID].email}`
+      };
+      res.render('url_not_found', errorMessage);
     }
   } else {
-    res.redirect('/urls');
+    // user is not logged in
+    const errorMessage = {
+      heading: 'Not Logged In'
+    };
+    res.render('url_not_found', errorMessage);
   }
 });
 
@@ -126,17 +165,25 @@ app.get('/u/:shortURL', (req, res) => {
   console.log(urlDatabase);
   if (urlDatabase[req.params.shortURL] !== undefined) {
     let longURL = urlDatabase[req.params.shortURL].longURL;
-    if (!longURL.toLowerCase().includes("http://") && !longURL.toLowerCase().includes("https://")) {
-      longURL = "http://" + longURL;
+    if (!longURL.toLowerCase().includes('http://') && !longURL.toLowerCase().includes('https://')) {
+      longURL = 'http://' + longURL;
     }
     res.redirect(longURL);
   } else {
-    res.status(404).render('url_not_found');
+    // URL for the given ID does not exist
+    const errorMessage = {
+      heading: 'URL Not Found'
+    };
+    res.status(404).render('url_not_found', errorMessage);
   }
 });
 
 app.get('/login', (req, res) => {
-  res.render('login');
+  if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
+    res.redirect(`/urls`);
+  } else {
+    res.render('login');
+  }
 });
 
 app.post('/login', (req, res) => {
@@ -145,16 +192,14 @@ app.post('/login', (req, res) => {
     if (doesUserExist && doesUserExist.email === req.body.email) {
       if (bcrypt.compareSync(req.body.password, doesUserExist.password)) {
         req.session.userID = doesUserExist.id;
-        res.redirect(`/urls`);
-      } else {
-        res.status(403).send("username or password incorrect");
+        return res.redirect(`/urls`);
       }
-    } else {
-      res.status(403).send("username or password incorrect");
     }
-  } else {
-    res.status(403).send("username or password incorrect");
   }
+  const errorMessage = {
+    heading: 'username or password incorrect'
+  };
+  res.status(403).render('url_not_found', errorMessage);
 });
 
 app.post('/logout', (req, res) => {
@@ -163,13 +208,20 @@ app.post('/logout', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  res.render('register');
+  if (req.session.userID !== undefined && users[req.session.userID] !== undefined) {
+    res.redirect(`/urls`);
+  } else {
+    res.render('register');
+  }
 });
 
 app.post('/register', (req, res) => {
   if (req.body.email !== undefined && req.body.password !== undefined) {
     if (getUserByEmail(req.body.email, users)) {
-      res.status(400).send('Email already registered');
+      const errorMessage = {
+        heading: 'Email already registered'
+      };
+      res.status(400).render('url_not_found', errorMessage);
     } else {
       const randomID = generateRandomString();
       users[randomID] = {
